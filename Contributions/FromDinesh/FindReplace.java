ed61840b30ed9b2730ac3ecbab8a95109e8b5bd1
adineskumar <adineskumar.isds@gmail.com>
2017-03-19 12:15:35 +0530

    FindReplace.java

---
+++ Contributions/FromDinesh/FindReplace.java
@@ -0,0 +1,66 @@
+/*
+ * Find a word and replace all the occurences of the word with different string
+ * and create a new file
+ */
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class FindReplace {
+
+  public static class FindReplaceMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+
+    	String str;
+    	if((str = value.toString()) != null && str.contains("dinesh") ) {
+    		word.set(str.replaceAll("dinesh","jagadish"));
+    		context.write(word, one);
+    	}
+    }
+  }
+
+  public static class FindReplaceReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "Find Replace");
+    job.setJarByClass(FindReplace.class);
+    job.setMapperClass(FindReplaceMapper.class);
+    job.setCombinerClass(FindReplaceReducer.class);
+    job.setReducerClass(FindReplaceReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
