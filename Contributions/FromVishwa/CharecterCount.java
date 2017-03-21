6c45cf425ef33a47966a823b80ffc4c79a83ea77
Vishwanath M L <vishwanathml.isds@gmail.com>
2017-03-22 00:05:43 +0530

    Add files via upload

---
+++ Contributions/FromVishwa/CharecterCount.java
@@ -0,0 +1,79 @@
+package com.vishwa.mr;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class CharecterCount extends Configured implements Tool {
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new Configuration(), new CharecterCount(),
+				args);
+		System.exit(res);
+	}
+
+	public int run(String[] args) throws Exception {
+		int reduce_tasks = 1;
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "wordcount using MapReduce");
+		job.setJarByClass(CharecterCount.class);
+		job.setNumReduceTasks(reduce_tasks);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setMapperClass(Map.class);
+		job.setCombinerClass(Reduce.class);
+		job.setReducerClass(Reduce.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.waitForCompletion(true);
+		return 0;
+	}
+
+	public static class Map extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
+		// private IntWritable one = new IntWritable(1);
+		private Text charInWord = new Text("Charecter Count");
+		private Text word = new Text();
+
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			int count;
+
+			String line = value.toString();
+			StringTokenizer tokens = new StringTokenizer(line);
+			while (tokens.hasMoreTokens()) {
+				word.set(tokens.nextToken());
+				count = word.toString().length();
+				context.write(charInWord, new IntWritable(count));
+			}
+		}
+	}
+
+	public static class Reduce extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+}
