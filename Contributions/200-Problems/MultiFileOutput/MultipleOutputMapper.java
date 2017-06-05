f00453bed9eac8dc36b48ed7cee78a28809a4c13
adineskumar <adineskumar.isds@gmail.com>
2017-06-05 10:47:39 +0530

    Add files via upload

---
+++ Contributions/200-Problems/MultiFileOutput/MultipleOutputMapper.java
@@ -0,0 +1,28 @@
+package multiFileOutputpkg;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+ 
+public class MultipleOutputMapper extends Mapper<LongWritable, Text, Text, Text> 
+	{
+		private Text txtKey = new Text("");
+		private Text txtValue = new Text("");
+  
+		@Override
+		protected void map(LongWritable key, Text value,Context context) throws IOException, InterruptedException 
+		{
+			if(value.toString().length() > 0) 
+			{
+				String[] custArray = value.toString().split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+				
+				txtKey.set(custArray[2].toString());
+				txtValue.set(custArray[0].toString()+"\t"+custArray[1].toString()+"\t"+custArray[3].toString());
+				
+				context.write(txtKey, txtValue);
+			}
+		}
+	}
+
