cd5acd6087ec1f874c5107ed1a9e9c0e2bbfd564
adineskumar <adineskumar.isds@gmail.com>
2017-07-15 17:32:36 +0530

    Create XMLMapper.java

---
+++ Contributions/200-Problems/Reading XML/XMLMapper.java
@@ -0,0 +1,60 @@
+package XML;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+ 
+public class XMLMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
+  
+    // Fprivate Text videoName = new Text();
+ 
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+ 
+        try {
+ 
+            InputStream is = new ByteArrayInputStream(value.toString().getBytes());
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(is);
+ 
+            doc.getDocumentElement().normalize();
+ 
+            NodeList nList = doc.getElementsByTagName("employee");
+ 
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+ 
+                Node nNode = nList.item(temp);
+ 
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+ 
+                    Element eElement = (Element) nNode;
+ 
+                    String id = eElement.getElementsByTagName("id").item(0).getTextContent();
+                    String name = eElement.getElementsByTagName("name").item(0).getTextContent();
+                    String gender = eElement.getElementsByTagName("gender").item(0).getTextContent();
+ 
+                    // System.out.println(id + "," + name + "," + gender);
+                    context.write(new Text(id + "," + name + "," + gender), NullWritable.get());
+ 
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());            
+        }
+ 
+    }
+ 
+}
